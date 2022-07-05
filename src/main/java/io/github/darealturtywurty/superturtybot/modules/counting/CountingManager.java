cc45cc8e46843dc4c1e6999ca08a9718bacfcd25
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-07-05 13:33:12 +0100

    Remove counting cache
    The counting mode was cached using a very temporary system which solved a temporary bug. I have now removed this however In the future I will re-add with a much better system so that constant calls to the database are not being made.

--- src/main/java/io/github/darealturtywurty/superturtybot/modules/counting/CountingManager.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/modules/counting/CountingManager.java
@@ -1,9 +1,7 @@
 package io.github.darealturtywurty.superturtybot.modules.counting;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -27,67 +25,54 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class CountingManager extends ListenerAdapter {
     public static final CountingManager INSTANCE = new CountingManager();
-    private final Map<Long, List<Long>> countingChannels = new HashMap<>();
-
+    
     public boolean isCountingChannel(Guild guild, TextChannel channel) {
-        if (!this.countingChannels.containsKey(guild.getIdLong())
-            || !this.countingChannels.get(guild.getIdLong()).contains(channel.getIdLong()))
-            return false;
-
         final Bson filter = Filters.and(Filters.eq("guild", guild.getIdLong()),
             Filters.eq("channel", channel.getIdLong()));
         final Counting profile = Database.getDatabase().counting.find(filter).first();
-        if (profile == null) {
-            this.countingChannels.get(guild.getIdLong()).remove(channel.getIdLong());
+        if (profile == null)
             return false;
-        }
-
+        
         return true;
     }
-    
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (shouldIgnore(event))
             return;
-        
+
         final TextChannel channel = event.getTextChannel();
-        
-        if (!this.countingChannels.containsKey(event.getGuild().getIdLong())
-            || !this.countingChannels.get(event.getGuild().getIdLong()).contains(channel.getIdLong()))
-            return;
-        
+
         final Bson filter = Filters.and(Filters.eq("guild", event.getGuild().getIdLong()),
             Filters.eq("channel", channel.getIdLong()));
         final Counting profile = Database.getDatabase().counting.find(filter).first();
-        if (profile == null) {
-            this.countingChannels.get(event.getGuild().getIdLong()).remove(channel.getIdLong());
+        if (profile == null)
             return;
-        }
-        
+
         final CountingMode mode = CountingMode.valueOf(profile.getCountingMode());
-        
+
         final List<Bson> updates = new ArrayList<>();
-        
+
         final Message message = event.getMessage();
         final User user = message.getAuthor();
         final String content = message.getContentRaw();
         final float given = CountingMode.parse(mode, content);
-        
+
         // If the provided value is not valid
         if (Float.isNaN(given))
             return;
-        
+
         // If the user is the latest counter
         if (profile.getLatestCounter() == user.getIdLong()) {
             final List<UserData> users = profile.getUsers();
             final Optional<UserData> optData = users.stream().filter(data -> data.getUser() == user.getIdLong())
                 .findFirst();
-            
+
             UserData data;
             // Check that the user has their data in the database
             if (optData.isPresent()) {
                 data = optData.get();
-                
+
                 // If the user's current count succession is already greater or equal to 3 then fail
                 if (data.getCurrentCountSuccession() >= 100) {
                     final float starting = CountingMode.getStartingNumber(mode);
@@ -103,7 +88,7 @@ public class CountingManager extends ListenerAdapter {
                 data = new UserData(user.getIdLong());
                 profile.getUsers().add(data);
             }
-            
+
             final int currentCountSuccession = data.getCurrentCountSuccession();
             users.forEach(u -> u.setCurrentCountSuccession(0));
             data.setCurrentCountSuccession(currentCountSuccession + 1);
@@ -111,7 +96,7 @@ public class CountingManager extends ListenerAdapter {
         } else {
             profile.setLatestCounter(user.getIdLong());
             updates.add(Updates.set("latestCounter", profile.getLatestCounter()));
-
+            
             final List<UserData> users = profile.getUsers();
             final Optional<UserData> optData = users.stream().filter(data -> data.getUser() == user.getIdLong())
                 .findFirst();
@@ -122,28 +107,28 @@ public class CountingManager extends ListenerAdapter {
                 data = new UserData(user.getIdLong());
                 profile.getUsers().add(data);
             }
-            
+
             final int currentCountSuccession = data.getCurrentCountSuccession();
             users.forEach(u -> u.setCurrentCountSuccession(0));
             data.setCurrentCountSuccession(currentCountSuccession + 1);
             data.setTotalCounts(data.getTotalCounts() + 1);
         }
-
+        
         final float currentNext = profile.getNextNumber();
         if (currentNext == given) {
             message.addReaction("✅").queue();
-
+            
             profile.setCurrentNumber(currentNext);
             updates.add(Updates.set("currentNumber", profile.getCurrentNumber()));
-
+            
             profile.setCurrentCount(profile.getCurrentCount() + 1);
             updates.add(Updates.set("currentCount", profile.getCurrentCount()));
-            
+
             if (mode == CountingMode.MATHS) {
                 final Pair<MathOperation, Float> maths = MathHandler.getNextNumber(currentNext);
                 profile.setNextNumber(maths.getRight());
                 updates.add(Updates.set("nextNumber", profile.getNextNumber()));
-
+                
                 if (mode.shouldNotify()) {
                     channel.sendMessage(profile.getCurrentCount() + ". The next number is: "
                         + MathHandler.parse(maths.getLeft(), currentNext, profile.getNextNumber())).queue();
@@ -151,19 +136,19 @@ public class CountingManager extends ListenerAdapter {
             } else {
                 profile.setNextNumber(CountingMode.getNextNumber(mode, currentNext));
                 updates.add(Updates.set("nextNumber", profile.getNextNumber()));
-
+                
                 if (mode.shouldNotify()) {
                     channel.sendMessage(profile.getCurrentCount() + ". The next number is: "
                         + CountingMode.parse(mode, currentNext, profile.getNextNumber())).queue();
                 }
             }
-
+            
             updates.add(Updates.set("users", profile.getUsers()));
             Database.getDatabase().counting.updateOne(filter, updates);
-            
+
             return;
         }
-
+        
         final float starting = CountingMode.getStartingNumber(mode);
         if (mode == CountingMode.MATHS) {
             final Pair<MathOperation, Float> next = MathHandler.getNextNumber(starting);
@@ -171,7 +156,7 @@ public class CountingManager extends ListenerAdapter {
             if (should.endsWith(".0")) {
                 should = should.substring(0, should.length() - 2);
             }
-            
+
             failChannel(profile, mode, message, filter, updates,
                 "❌ `" + content + "` is not the correct number! It should have been `" + should
                     + "`. The next number is: **" + MathHandler.parse(next.getLeft(), starting, next.getRight())
@@ -179,65 +164,59 @@ public class CountingManager extends ListenerAdapter {
                 starting, next.getRight());
             return;
         }
-        
+
         final float next = CountingMode.getNextNumber(mode, starting);
         String should = String.valueOf(currentNext);
         if (should.endsWith(".0")) {
             should = should.substring(0, should.length() - 2);
         }
-
+        
         failChannel(profile, mode, message, filter, updates,
             "❌ `" + content + "` is not the correct number! It should have been `" + should
                 + "`. The next number is: **" + CountingMode.parse(mode, starting, next) + "**.",
             starting, next);
     }
-
+    
     public boolean removeCountingChannel(Guild guild, TextChannel channel) {
         if (!isCountingChannel(guild, channel))
             return false;
-
+        
         final Bson filter = Filters.and(Filters.eq("guild", guild.getIdLong()),
             Filters.eq("channel", channel.getIdLong()));
         return Database.getDatabase().counting.deleteOne(filter).getDeletedCount() > 0;
     }
-
+    
     public boolean setCountingChannel(Guild guild, TextChannel channel, CountingMode mode) {
         if (isCountingChannel(guild, channel))
             return false;
-        
-        if (this.countingChannels.containsKey(guild.getIdLong())) {
-            this.countingChannels.get(guild.getIdLong()).add(channel.getIdLong());
-        } else {
-            this.countingChannels.put(guild.getIdLong(), new ArrayList<>(List.of(channel.getIdLong())));
-        }
-        
+
         return Database.getDatabase().counting.insertOne(new Counting(guild.getIdLong(), channel.getIdLong(), mode))
             .getInsertedId() != null;
     }
-
+    
     private void failChannel(final Counting profile, final CountingMode mode, final Message message, final Bson filter,
         final List<Bson> updates, String response, float startAt, float nextNumber) {
         final List<UserData> users = profile.getUsers();
         users.forEach(u -> u.setCurrentCountSuccession(0));
         updates.add(Updates.set("users", users));
-        
+
         profile.setCurrentCount(0);
         updates.add(Updates.set("currentCount", profile.getCurrentCount()));
-        
+
         profile.setLatestCounter(0L);
         updates.add(Updates.set("latestCounter", profile.getLatestCounter()));
-
+        
         profile.setCurrentNumber(startAt);
         updates.add(Updates.set("currentNumber", profile.getCurrentNumber()));
-        
+
         profile.setNextNumber(nextNumber);
         updates.add(Updates.set("nextNumber", profile.getNextNumber()));
-        
+
         Database.getDatabase().counting.updateOne(filter, updates);
-        
+
         message.addReaction("💀").queue(success -> message.reply(response).mentionRepliedUser(false).queue());
     }
-
+    
     private boolean shouldIgnore(MessageReceivedEvent event) {
         return !event.isFromGuild() || event.isFromThread() || event.isWebhookMessage() || event.getAuthor().isBot()
             || event.getAuthor().isSystem() || event.getChannelType() != ChannelType.TEXT;
