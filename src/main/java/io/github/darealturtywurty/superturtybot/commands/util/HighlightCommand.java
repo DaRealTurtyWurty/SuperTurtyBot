c1835cfcf9824cfdaffbb4f8991f024804b37268
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-07-28 19:36:26 +0100

    Highlighter autocomplete

--- src/main/java/io/github/darealturtywurty/superturtybot/commands/util/HighlightCommand.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/commands/util/HighlightCommand.java
@@ -2,6 +2,7 @@ package io.github.darealturtywurty.superturtybot.commands.util;
 
 import java.awt.Color;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -19,6 +20,7 @@ import io.github.darealturtywurty.superturtybot.database.Database;
 import io.github.darealturtywurty.superturtybot.database.pojos.collections.Highlighter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -29,7 +31,7 @@ public class HighlightCommand extends CoreCommand {
     public HighlightCommand() {
         super(new Types(true, false, false, false));
     }
-    
+
     @Override
     public List<SubcommandData> createSubcommands() {
         return List.of(
@@ -39,47 +41,67 @@ public class HighlightCommand extends CoreCommand {
                     "Whether or not this highlighter is case sensitive (default: false)", false),
             new SubcommandData("list", "Lists your current highlighters"),
             new SubcommandData("delete", "Deletes an existing highlighter").addOption(OptionType.STRING, "id",
-                "The ID of the highlighter that you want to delete", true));
+                "The ID of the highlighter that you want to delete", true, true));
     }
-
+    
     @Override
     public CommandCategory getCategory() {
         return CommandCategory.UTILITY;
     }
-    
+
     @Override
     public String getDescription() {
         return "Notifies you every time a message containing your specified text is sent.";
     }
-    
+
     @Override
     public String getHowToUse() {
         return "/highlight create [text]\n/highlight create [text] [caseSensitive]\nhighlight list\n/highlight delete [id]";
     }
-    
+
     @Override
     public String getName() {
         return "highlight";
     }
-    
+
     @Override
     public String getRichName() {
         return "Highlight";
     }
-    
+
     @Override
     public boolean isServerOnly() {
         return true;
     }
-    
+
+    @Override
+    public void onCommandAutoCompleteInteraction(CommandAutoCompleteInteractionEvent event) {
+        if (!event.isFromGuild() || !event.getName().equals(getName()))
+            return;
+
+        final String subcommand = event.getSubcommandName();
+        if (!"delete".equals(subcommand))
+            return;
+
+        final String term = event.getFocusedOption().getValue();
+
+        final Bson filter = Filters.and(Filters.eq("guild", event.getGuild().getIdLong()),
+            Filters.eq("user", event.getUser().getIdLong()));
+        final List<Highlighter> highlighters = new ArrayList<>();
+        Database.getDatabase().highlighters.find(filter).forEach(highlighters::add);
+        final List<String> options = highlighters.stream().filter(highlighter -> highlighter.getUuid().contains(term))
+            .limit(25).map(Highlighter::getUuid).toList();
+        event.replyChoiceStrings(options).queue();
+    }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (!event.isFromGuild() || event.isWebhookMessage() || event.getAuthor().isBot()
             || event.getAuthor().isSystem())
             return;
-        
+
         final Bson filter = Filters.eq("guild", event.getGuild().getIdLong());
-        
+
         final String content = event.getMessage().getContentRaw();
         for (final Highlighter highlighter : Database.getDatabase().highlighters.find(filter)) {
             event.getGuild().retrieveMemberById(highlighter.getUser()).queue(
@@ -87,67 +109,67 @@ public class HighlightCommand extends CoreCommand {
                 error -> highlightFailed(event, content, highlighter, error));
         }
     }
-    
+
     @Override
     protected void runSlash(SlashCommandInteractionEvent event) {
         if (!event.isFromGuild()) {
             reply(event, "❌ This command can only be used inside a server!", false, true);
             return;
         }
-        
+
         switch (event.getSubcommandName()) {
             case "create": {
                 final String text = event.getOption("text").getAsString();
                 final boolean caseSensitive = event.getOption("case_sensitive", false, OptionMapping::getAsBoolean);
-                
+
                 createHighlighter(event, text, caseSensitive);
                 return;
             }
-            
+
             case "list": {
                 final Set<Highlighter> highlighters = new HashSet<>();
                 final Bson filter = Filters.and(Filters.eq("guild", event.getGuild().getIdLong()),
                     Filters.eq("user", event.getUser().getIdLong()));
                 Database.getDatabase().highlighters.find(filter).forEach(highlighters::add);
-                
+
                 if (highlighters.isEmpty()) {
                     reply(event, "❌ You have no highlighters!", false, true);
                     return;
                 }
-                
+
                 listHighlighters(event, highlighters);
                 return;
             }
-            
+
             case "delete": {
                 final String id = event.getOption("id").getAsString();
-                
+
                 final Bson filter = Filters.and(Filters.eq("guild", event.getGuild().getIdLong()),
                     Filters.eq("user", event.getUser().getIdLong()), Filters.eq("uuid", id));
                 final Highlighter highlighter = Database.getDatabase().highlighters.find(filter).first();
-                
+
                 if (highlighter == null) {
                     reply(event, "❌ You do not have a highlighter with this ID!", false, true);
                     return;
                 }
-                
+
                 deleteHighlighter(event, filter, highlighter);
                 return;
             }
-            
+
             default: {
                 reply(event, "❌ You must provide a valid subcommand (`create`, `list`, `delete`)!", false, true);
                 break;
             }
         }
     }
-    
+
     private static void createHighlighter(SlashCommandInteractionEvent event, final String text,
         final boolean caseSensitive) {
         final Highlighter highlighter = new Highlighter(event.getGuild().getIdLong(), event.getUser().getIdLong(), text,
             caseSensitive);
         Database.getDatabase().highlighters.insertOne(highlighter);
-        
+
         final var embed = new EmbedBuilder();
         embed.setTimestamp(Instant.now());
         embed.setColor(Color.GREEN);
@@ -155,10 +177,10 @@ public class HighlightCommand extends CoreCommand {
         embed.setFooter("ID: " + highlighter.asUUID(), event.getUser().getEffectiveAvatarUrl());
         event.deferReply().addEmbeds(embed.build()).mentionRepliedUser(false).queue();
     }
-    
+
     private static void deleteHighlighter(SlashCommandInteractionEvent event, Bson filter, Highlighter highlighter) {
         Database.getDatabase().highlighters.deleteOne(filter);
-        
+
         final var embed = new EmbedBuilder();
         embed.setTimestamp(Instant.now());
         embed.setColor(Color.RED);
@@ -167,7 +189,7 @@ public class HighlightCommand extends CoreCommand {
         embed.setFooter("ID: " + highlighter.asUUID(), event.getUser().getEffectiveAvatarUrl());
         event.deferReply().addEmbeds(embed.build()).mentionRepliedUser(false).queue();
     }
-    
+
     private static void highlightFailed(MessageReceivedEvent event, final String content, final Highlighter highlighter,
         Throwable error) {
         Constants.LOGGER.error(
@@ -175,7 +197,7 @@ public class HighlightCommand extends CoreCommand {
             event.getChannel().getIdLong(), event.getGuild().getIdLong(), event.getMessageId(), error.getMessage(),
             ExceptionUtils.getMessage(error), error.getMessage(), ExceptionUtils.getMessage(error));
     }
-    
+
     private static void listHighlighters(SlashCommandInteractionEvent event, Set<Highlighter> highlighters) {
         final var embed = new EmbedBuilder();
         embed.setTimestamp(Instant.now());
@@ -183,7 +205,7 @@ public class HighlightCommand extends CoreCommand {
         embed.setTitle("**" + event.getUser().getName() + "'s Highlighters:**");
         final List<Highlighter> guildHighlighters = highlighters.stream()
             .sorted(Comparator.comparing(Highlighter::getTimeAdded)).toList();
-        
+
         boolean none = true;
         if (!guildHighlighters.isEmpty()) {
             final var builder = new StringBuilder();
@@ -193,19 +215,19 @@ public class HighlightCommand extends CoreCommand {
             embed.appendDescription(builder.toString());
             none = false;
         }
-        
+
         if (none) {
             embed.addField("N/A", "N/A", false);
         }
-        
+
         event.deferReply().addEmbeds(embed.build()).mentionRepliedUser(false).queue();
     }
-    
+
     private static void performHighlight(MessageReceivedEvent event, final String content,
         final Highlighter highlighter, Member member) {
         if (!member.hasAccess(event.getGuildChannel()))
             return;
-        
+
         if (highlighter.isCaseSensitive() ? content.contains(highlighter.getText())
             : content.toLowerCase().contains(highlighter.getText().toLowerCase())) {
             member.getUser().openPrivateChannel()
@@ -217,7 +239,7 @@ public class HighlightCommand extends CoreCommand {
                         .queue());
         }
     }
-    
+
     // TODO: Utility class
     private static String truncateString(String str, int length) {
         if (str.length() > length)
