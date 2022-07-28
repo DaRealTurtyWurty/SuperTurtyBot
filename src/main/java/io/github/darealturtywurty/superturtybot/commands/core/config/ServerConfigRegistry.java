5ce442b8bc597d90efac2f9945b192aa86484224
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-07-28 15:57:47 +0100

    Add mod logging to server config registry

--- src/main/java/io/github/darealturtywurty/superturtybot/commands/core/config/ServerConfigRegistry.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/commands/core/config/ServerConfigRegistry.java
@@ -18,35 +18,35 @@ public class ServerConfigRegistry {
         TextChannel channel = guild.getTextChannelById(str);
         if (channel != null)
             return true;
-
+        
         if (MentionType.CHANNEL.getPattern().matcher(str).matches()) {
             final String id = str.replace("<#", "").replace(">", "");
             channel = guild.getTextChannelById(id);
             return channel != null;
         }
-
+        
         List<TextChannel> possibleMatches = guild.getTextChannelsByName(str, false);
         if (possibleMatches.isEmpty()) {
             possibleMatches = guild.getTextChannelsByName(str, true);
             if (possibleMatches.isEmpty())
                 return false;
         }
-
+        
         return true;
     };
-
-    public static final Registry<ServerConfigOption> SERVER_CONFIG_OPTIONS = new Registry<>();
     
+    public static final Registry<ServerConfigOption> SERVER_CONFIG_OPTIONS = new Registry<>();
+
     public static final ServerConfigOption STARBOARD = SERVER_CONFIG_OPTIONS.register("starboard",
         new ServerConfigOption.Builder().dataType(DataType.LONG)
             .serializer((config, value) -> config.setStarboard(Long.parseLong(value)))
             .valueFromConfig(GuildConfig::getStarboard).validator(TEXT_CHANNEL_VALIDATOR).build());
-    
+
     public static final ServerConfigOption IS_STARBOARD_ENABLED = SERVER_CONFIG_OPTIONS.register("starboard_enabled",
         new ServerConfigOption.Builder().dataType(DataType.BOOLEAN)
             .serializer((config, value) -> config.setStarboardEnabled(Boolean.parseBoolean(value)))
             .valueFromConfig(GuildConfig::isStarboardEnabled).build());
-    
+
     public static final ServerConfigOption MINIMUM_STARS = SERVER_CONFIG_OPTIONS.register("minimum_stars",
         new ServerConfigOption.Builder().dataType(DataType.INTEGER)
             .serializer((config, value) -> config.setMinimumStars(Integer.parseInt(value)))
@@ -54,9 +54,14 @@ public class ServerConfigRegistry {
                 final int input = Integer.parseInt(str);
                 return input >= 1 && input <= event.getGuild().getMemberCount();
             }).build());
-    
+
     public static final ServerConfigOption DO_BOT_STARS_COUNT = SERVER_CONFIG_OPTIONS.register("bot_stars_count",
         new ServerConfigOption.Builder().dataType(DataType.BOOLEAN)
             .serializer((config, value) -> config.setBotStarsCount(Boolean.parseBoolean(value)))
             .valueFromConfig(GuildConfig::isBotStarsCount).build());
+
+    public static final ServerConfigOption MOD_LOGGING = SERVER_CONFIG_OPTIONS.register("mod_logging",
+        new ServerConfigOption.Builder().dataType(DataType.LONG)
+            .serializer((config, value) -> config.setModLogging(Long.parseLong(value)))
+            .valueFromConfig(GuildConfig::getModLogging).validator(TEXT_CHANNEL_VALIDATOR).build());
 }
