d90d933e72ecca69fb351fd886266b25a032651e
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-07-28 19:34:56 +0100

    Re-enable cooldown and fix rank command

--- src/main/java/io/github/darealturtywurty/superturtybot/commands/levelling/RankCommand.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/commands/levelling/RankCommand.java
@@ -49,7 +49,7 @@ public class RankCommand extends CoreCommand {
     private static final char[] CHARS = { 'k', 'm', 'b', 't' };
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("##.#");
     private final Font usedFont;
-    
+
     public RankCommand() {
         super(new Types(true, false, false, false));
         final var graphicsEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -60,41 +60,44 @@ public class RankCommand extends CoreCommand {
         } catch (FontFormatException | IOException exception) {
             throw new IllegalStateException("Unable to load font", exception);
         }
-        
+
         graphicsEnv.registerFont(this.usedFont);
     }
-    
+
     @Override
     public List<OptionData> createOptions() {
         return List.of(new OptionData(OptionType.USER, "member", "The member to gain rank information about", false));
     }
-    
+
     @Override
     public CommandCategory getCategory() {
         return CommandCategory.LEVELLING;
     }
-    
+
     @Override
     public String getDescription() {
         return "Gets the rank of yourself or the provided member";
     }
-
+    
     @Override
     public String getHowToUse() {
         return "/rank\n/rank [user]";
     }
-    
+
     @Override
     public String getName() {
         return "rank";
     }
-    
+
     public int getRank(Member member) {
-        final Bson filter = Filters.eq("guild", member.getGuild());
+        if (member.getUser().isBot())
+            return 0;
+
+        final Bson filter = Filters.eq("guild", member.getGuild().getIdLong());
         List<Levelling> profiles = new ArrayList<>();
         Database.getDatabase().levelling.find(filter).forEach(profiles::add);
-        profiles = profiles.stream().sorted(Comparator.comparing(Levelling::getXp)).toList();
-        
+        profiles = profiles.stream().sorted(Comparator.comparing(Levelling::getXp).reversed()).toList();
+
         final Optional<Levelling> found = profiles.stream().filter(profile -> profile.getUser() == member.getIdLong())
             .findFirst();
         if (!found.isPresent()) {
@@ -102,20 +105,20 @@ public class RankCommand extends CoreCommand {
             Database.getDatabase().levelling.insertOne(profile);
             return profiles.size() + 1;
         }
-        
-        return profiles.indexOf(found.get()) + 1;
+
+        return profiles.indexOf(found.get());
     }
-    
+
     @Override
     public String getRichName() {
         return "Rank";
     }
-    
+
     @Override
     public boolean isServerOnly() {
         return true;
     }
-    
+
     @Override
     protected void runSlash(SlashCommandInteractionEvent event) {
         if (!event.isFromGuild()) {
@@ -123,10 +126,10 @@ public class RankCommand extends CoreCommand {
                 .queue();
             return;
         }
-        
+
         final Member member = event.getOption("member", event.getMember(),
             option -> event.getGuild().getMember(option.getAsUser()));
-        
+
         if (member == null) {
             event.deferReply(true)
                 .setContent(
@@ -135,20 +138,25 @@ public class RankCommand extends CoreCommand {
             return;
         }
         
+        if (member.getUser().isBot()) {
+            reply(event, "❌ Bots cannot level up. Silly!");
+            return;
+        }
+
         if (!LevellingManager.INSTANCE.areLevelsEnabled(event.getGuild())) {
             event.deferReply(true).setContent("Levels have been disabled for this server!").mentionRepliedUser(false)
                 .queue();
             return;
         }
-        
+
         final Bson filter = Filters.and(Filters.eq("guild", event.getGuild().getIdLong()),
-            Filters.eq("user", event.getUser().getIdLong()));
+            Filters.eq("user", member.getIdLong()));
         Levelling profile = Database.getDatabase().levelling.find(filter).first();
         if (profile == null) {
-            profile = new Levelling(event.getGuild().getIdLong(), event.getUser().getIdLong());
+            profile = new Levelling(event.getGuild().getIdLong(), member.getIdLong());
             Database.getDatabase().levelling.insertOne(profile);
         }
-        
+
         final int level = profile.getLevel();
         final int xp = profile.getXp();
         final int forLevel = LevellingManager.getXPForLevel(level + 1);
@@ -157,15 +165,15 @@ public class RankCommand extends CoreCommand {
         if (percentage < 0) {
             percentage = 0;
         }
-        
+
         final File card = makeRankCard(member, profile.getRankCard(), level, xp, forLevel, percentage);
         event.deferReply().addFile(card).mentionRepliedUser(false).queue();
     }
-    
+
     @Nullable
     private File makeRankCard(final Member member, RankCard card, int level, int xp, int nextLevelXp, float xpPercent) {
         final var location = new File("/levels/cards/" + member.getIdLong() + ".png");
-        
+
         try {
             final BufferedImage base = ImageIO
                 .read(TurtyBot.class.getResourceAsStream("/levels/background/default.png"));
@@ -179,7 +187,7 @@ public class RankCommand extends CoreCommand {
             graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             // graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
             // RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT);
-            
+
             // Background
             BufferedImage background;
             if (!card.getBackgroundImage().isBlank()) {
@@ -193,14 +201,14 @@ public class RankCommand extends CoreCommand {
                 bgGraphics.dispose();
                 background = bgBuf;
             }
-            
+
             final var bgBuffer = new BufferedImage(base.getWidth(), base.getHeight(), BufferedImage.TYPE_INT_ARGB);
             final var bgGraphics = bgBuffer.createGraphics();
             bgGraphics.setClip(new Rectangle2D.Float(0, 0, base.getWidth(), base.getHeight()));
             bgGraphics.drawImage(background, 0, 0, base.getWidth(), base.getHeight(), null);
             bgGraphics.dispose();
             graphics.drawImage(bgBuffer, 0, 0, base.getWidth(), base.getHeight(), null);
-            
+
             // Outline
             BufferedImage outlineImg;
             if (!card.getOutlineImage().isBlank()) {
@@ -215,69 +223,69 @@ public class RankCommand extends CoreCommand {
                 outGraphics.dispose();
                 outlineImg = outBuf;
             }
-            
+
             outlineImg = cutoutImageMiddle(outlineImg, base.getWidth(), base.getHeight(), 20);
-            
+
             final var outlineAlpha = card.getOutlineOpacity();
             final var alphaComp = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, outlineAlpha);
             graphics.setComposite(alphaComp);
             graphics.drawImage(outlineImg, 0, 0, base.getWidth(), base.getHeight(), null);
             graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-            
+
             // Name
             graphics.setStroke(new BasicStroke(3));
             graphics.setColor(card.getNameTextColor().asColor());
-            
+
             final String name = member.getUser().getName();
             final var nameFontSize = name.length() <= 12 ? 200f : 112f;
             graphics.setFont(this.usedFont.deriveFont(nameFontSize));
-            
+
             graphics.drawString(name.length() > 26 ? name.substring(0, 26) + "..." : name, 700, 270);
-            
+
             // Rank
             graphics.setColor(card.getRankTextColor().asColor());
             graphics.setFont(this.usedFont.deriveFont(105f));
-            
+
             int rank = getRank(member);
-            
+
             var xModifier = 0;
             if (rank >= 10) {
                 xModifier += 15;
             }
-            
+
             if (rank >= 100) {
                 xModifier += 15;
             }
-            
+
             if (rank >= 1000) {
                 xModifier += 15;
             }
-            
+
             if (rank >= 10000) {
                 xModifier += 15;
             }
-            
+
             if (rank < 0) {
                 rank = 0;
             }
-            
+
             graphics.drawString("Rank #" + (rank + 1), 2200 - xModifier, 250);
-            
+
             final var fontMetrics = graphics.getFontMetrics();
             final int textWidth = fontMetrics.stringWidth("Rank #" + (rank + 1));
             graphics.drawLine(700, 300 + fontMetrics.getDescent(), 2200 - xModifier + textWidth,
                 300 + fontMetrics.getDescent());
-            
+
             final var xpStr = xpFormat(xp, 0).replace(".0", "");
             final var levelStr = String.valueOf(level);
             final var nextLevelXPStr = xpFormat(nextLevelXp, 0).replace(".0", "");
             graphics.setColor(card.getLevelTextColor().asColor());
             graphics.drawString("Level " + levelStr, 700, 450);
             graphics.setFont(this.usedFont.deriveFont(75f));
-            
+
             graphics.setColor(card.getXpTextColor().asColor());
             drawRightAlignedString(graphics, xpStr + " / " + nextLevelXPStr, this.usedFont.deriveFont(75f), 2600, 375);
-            
+
             // XP Bar
             if (!card.getXpOutlineImage().isBlank()) {
                 final BufferedImage xpBarOutline = makeRoundedCorner(
@@ -289,7 +297,7 @@ public class RankCommand extends CoreCommand {
                 graphics.setColor(card.getXpOutlineColor().asColor());
                 graphics.drawRoundRect(700, 550, 1900, 120, 30, 30);
             }
-            
+
             if (!card.getXpEmptyImage().isBlank()) {
                 final BufferedImage xpBarEmpty = makeRoundedCorner(ImageIO.read(
                     TurtyBot.class.getResourceAsStream("/levels/xpEmpty/" + card.getXpEmptyImage() + ".png")), 20);
@@ -298,7 +306,7 @@ public class RankCommand extends CoreCommand {
                 graphics.setColor(card.getXpEmptyColor().asColor());
                 graphics.fillRoundRect(700, 550, 1900, 120, 30, 30);
             }
-            
+
             if (!card.getXpFillImage().isBlank()) {
                 final BufferedImage xpBarFill = makeRoundedCorner(ImageIO
                     .read(TurtyBot.class.getResourceAsStream("/levels/xpFill/" + card.getXpFillImage() + ".png")), 20);
@@ -307,16 +315,16 @@ public class RankCommand extends CoreCommand {
                 graphics.setColor(card.getXpFillColor().asColor());
                 graphics.fillRoundRect(700, 550, (int) (1900 * (xpPercent * 0.01f)), 120, 30, 30);
             }
-            
+
             graphics.setColor(card.getPercentTextColor().asColor());
             graphics.setFont(this.usedFont.deriveFont(90f));
-            
+
             graphics.drawString(DECIMAL_FORMAT.format(xpPercent) + "%", 1500, 640);
-            
+
             // User Avatar
             BufferedImage userAvatar = ImageIO.read(new URL(member.getUser().getEffectiveAvatarUrl()));
             userAvatar = resize(userAvatar, 512);
-            
+
             if (!card.getAvatarOutlineImage().isBlank()) {
                 final var avatarOut = new BufferedImage(userAvatar.getWidth(), userAvatar.getHeight(),
                     BufferedImage.TYPE_INT_ARGB);
@@ -337,21 +345,21 @@ public class RankCommand extends CoreCommand {
                 graphics.fill(shape);
                 graphics.draw(shape);
             }
-            
+
             graphics.dispose();
-            
+
             if (!location.exists()) {
                 location.mkdirs();
             }
-            
+
             ImageIO.write(rankCardBuffer, "png", location);
         } catch (final IOException e) {
             e.printStackTrace();
         }
-        
+
         return location;
     }
-    
+
     public static Shape createRingShape(double centerX, double centerY, double outerRadius, double thickness) {
         final Ellipse2D outer = new Ellipse2D.Double(centerX - outerRadius, centerY - outerRadius,
             outerRadius + outerRadius, outerRadius + outerRadius);
@@ -362,11 +370,11 @@ public class RankCommand extends CoreCommand {
         area.subtract(new Area(inner));
         return area;
     }
-    
+
     public static BufferedImage cutoutImageMiddle(final BufferedImage image, final int baseWidth, final int baseHeight,
         final int cornerRadius) {
         final var output = new BufferedImage(baseWidth, baseHeight, BufferedImage.TYPE_INT_ARGB);
-        
+
         final var g2 = output.createGraphics();
         final var area = new Area(new Rectangle2D.Double(0, 0, baseWidth, baseHeight));
         final var toSubtract = new Area(new RoundRectangle2D.Double(cornerRadius, cornerRadius,
@@ -377,34 +385,34 @@ public class RankCommand extends CoreCommand {
         g2.dispose();
         return output;
     }
-    
+
     public static final float drawRightAlignedString(Graphics2D g, String string, Font font, float x, float y) {
         final float startX = x - getTextWidth(g, string, font);
         return drawString(g, string, font, startX, y);
     }
-    
+
     public static final float drawString(Graphics2D g, String string, Font font, float x, float y) {
         g.setFont(font);
         y += font.getLineMetrics(string, g.getFontRenderContext()).getAscent();
         g.drawString(string, x, y);
         return getTextHeight(g, string, font);
     }
-    
+
     public static final float getTextHeight(Graphics2D g, String string, Font font) {
         final LineMetrics lm = font.getLineMetrics(string, g.getFontRenderContext());
         return lm.getHeight();
     }
-    
+
     public static final float getTextWidth(Graphics2D g, String string, Font font) {
         return g.getFontMetrics(font).stringWidth(string);
     }
-    
+
     public static BufferedImage makeRoundedCorner(final BufferedImage image, final float cornerRadius) {
         final int w = image.getWidth();
         final int h = image.getHeight();
         final var output = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         final var g2 = output.createGraphics();
-        
+
         g2.setComposite(AlphaComposite.Src);
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(Color.WHITE);
@@ -412,41 +420,41 @@ public class RankCommand extends CoreCommand {
         g2.setComposite(AlphaComposite.SrcAtop);
         g2.drawImage(image, 0, 0, null);
         g2.dispose();
-        
+
         return output;
     }
-    
+
     public static void paintTextWithOutline(final Graphics g, final String text, final Font font,
         final Color outlineColor, final Color fillColor, final float outlineWidth) {
         final var outlineStroke = new BasicStroke(outlineWidth);
-        
+
         if (g instanceof final Graphics2D g2) {
             // remember original settings
             final var originalColor = g2.getColor();
             final var originalStroke = g2.getStroke();
             final var originalHints = g2.getRenderingHints();
-            
+
             // create a glyph vector from your text
             final var glyphVector = font.createGlyphVector(g2.getFontRenderContext(), text);
             // get the shape object
             final var textShape = glyphVector.getOutline();
-            
+
             g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-            
+
             g2.setColor(outlineColor);
             g2.setStroke(outlineStroke);
             g2.draw(textShape); // draw outline
-            
+
             g2.setColor(fillColor);
             g2.fill(textShape); // fill the shape
-            
+
             // reset to original settings after painting
             g2.setColor(originalColor);
             g2.setStroke(originalStroke);
             g2.setRenderingHints(originalHints);
         }
     }
-    
+
     /**
      * Takes a BufferedImage and resizes it according to the provided targetSize
      *
@@ -466,7 +474,7 @@ public class RankCommand extends CoreCommand {
         } else { // portrait image
             targetWidth = Math.round(targetHeight / ratio);
         }
-        
+
         final BufferedImage retImg = new BufferedImage(targetWidth, targetHeight,
             src.getTransparency() == Transparency.OPAQUE ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB);
         final Graphics2D g2d = retImg.createGraphics();
@@ -475,7 +483,7 @@ public class RankCommand extends CoreCommand {
         g2d.dispose();
         return retImg;
     }
-    
+
     private static void drawAvatar(final BufferedImage userAvatar, final Graphics2D graphics) {
         graphics.setStroke(new BasicStroke(4));
         final var circleBuffer = new BufferedImage(userAvatar.getWidth(), userAvatar.getHeight(),
@@ -486,7 +494,7 @@ public class RankCommand extends CoreCommand {
         avatarGraphics.dispose();
         graphics.drawImage(circleBuffer, 100, 50, null);
     }
-    
+
     /**
      * Recursive implementation, invokes itself for each factor of a thousand, increasing the class on each invokation.
      *
