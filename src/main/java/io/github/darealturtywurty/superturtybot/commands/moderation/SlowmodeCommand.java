2690efb7d3ed38163de2bad5939428931b426284
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-07-28 15:55:22 +0100

    Slowmode duration choices

--- src/main/java/io/github/darealturtywurty/superturtybot/commands/moderation/SlowmodeCommand.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/commands/moderation/SlowmodeCommand.java
@@ -17,48 +17,52 @@ public class SlowmodeCommand extends CoreCommand {
     public SlowmodeCommand() {
         super(new Types(true, false, false, false));
     }
-    
+
     @Override
     public List<OptionData> createOptions() {
         return List.of(new OptionData(OptionType.INTEGER, "time", "How long the cooldown is (in seconds)", false)
-            .setRequiredRange(0, 21600));
+            .setRequiredRange(0, 21600).addChoice("5 seconds", 5).addChoice("10 seconds", 10)
+            .addChoice("15 seconds", 15).addChoice("30 seconds", 30).addChoice("1 minute", 60)
+            .addChoice("2 minutes", 120).addChoice("5 minutes", 300).addChoice("10 minutes", 600)
+            .addChoice("15 minutes", 900).addChoice("30 minutes", 1800).addChoice("1 hour", 3600)
+            .addChoice("2 hours", 7200).addChoice("6 hours", 21600));
     }
-    
+
     @Override
     public String getAccess() {
         return "Moderators (Manage Channel Permission)";
     }
-
+    
     @Override
     public CommandCategory getCategory() {
         return CommandCategory.UTILITY;
     }
-    
+
     @Override
     public String getDescription() {
         return "Puts the current channel on slowmode";
     }
-
+    
     @Override
     public String getHowToUse() {
         return "/slowmode\n/slowmode [time]";
     }
-    
+
     @Override
     public String getName() {
         return "slowmode";
     }
-    
+
     @Override
     public String getRichName() {
         return "Slowmode";
     }
-    
+
     @Override
     public boolean isServerOnly() {
         return true;
     }
-    
+
     @Override
     protected void runSlash(SlashCommandInteractionEvent event) {
         if (!event.isFromGuild() || event.getChannelType() != ChannelType.TEXT) {
@@ -66,7 +70,7 @@ public class SlowmodeCommand extends CoreCommand {
                 .mentionRepliedUser(false).queue();
             return;
         }
-        
+
         final int time = event.getOption("time", 5, OptionMapping::getAsInt);
         event.getChannel().asTextChannel().getManager().setSlowmode(time).queue();
         event.deferReply().setContent("I have changed this channel's slowmode cooldown to " + time + " seconds!")
