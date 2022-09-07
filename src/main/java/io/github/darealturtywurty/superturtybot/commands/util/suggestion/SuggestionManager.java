862b227bc5481756946eb19611a1fa663d84bf91
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-09-07 17:49:12 +0100

    Fix suggestions for when there is no channel

--- src/main/java/io/github/darealturtywurty/superturtybot/commands/util/suggestion/SuggestionManager.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/commands/util/suggestion/SuggestionManager.java
@@ -32,37 +32,37 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public final class SuggestionManager extends ListenerAdapter {
     public static final SuggestionManager INSTANCE = new SuggestionManager();
-
+    
     private SuggestionManager() {
     }
-
+    
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
         if (!event.isFromGuild() || event.getUser().isBot() || event.getUser().isSystem())
             return;
-
+        
         final TextChannel suggestionChannel = getSuggestionChannel(event.getGuild());
         if (suggestionChannel == null || event.getChannel().getIdLong() != suggestionChannel.getIdLong())
             return;
-
+        
         final Bson filter = Filters.and(Filters.eq("guild", event.getGuild().getIdLong()),
             Filters.eq("message", event.getMessageIdLong()));
         final Suggestion suggestion = Database.getDatabase().suggestions.find(filter).first();
         if (suggestion == null)
             return;
-
+        
         event.getChannel().retrieveMessageById(suggestion.getMessage()).queue(msg -> {
             msg.addReaction(Emoji.fromUnicode("⬆️")).queue();
             msg.addReaction(Emoji.fromUnicode("⬇️")).queue();
         }, error -> Database.getDatabase().suggestions.deleteOne(filter));
     }
-
+    
     public static CompletableFuture<Suggestion> addSuggestion(TextChannel suggestionChannel, Guild guild,
         Member suggester, String content, @Nullable String mediaUrl) {
         final var counter = new AtomicInteger();
         Database.getDatabase().suggestions.find(Filters.eq("guild", guild.getIdLong()))
             .forEach(suggestion -> counter.getAndIncrement());
-
+        
         final var embed = new EmbedBuilder();
         embed.setTimestamp(Instant.now());
         embed.setColor(Color.GRAY);
@@ -74,7 +74,7 @@ public final class SuggestionManager extends ListenerAdapter {
             embed.setImage(mediaUrl);
             embed.appendDescription("\n\n" + "**Media not showing? [Click Me](" + mediaUrl + ")**");
         }
-
+        
         final var future = new CompletableFuture<Suggestion>();
         suggestionChannel.sendMessageEmbeds(embed.build()).queue(msg -> {
             final var suggestion = new Suggestion(guild.getIdLong(), suggester.getIdLong(), msg.getIdLong(),
@@ -83,27 +83,29 @@ public final class SuggestionManager extends ListenerAdapter {
             future.complete(suggestion);
             msg.addReaction(Emoji.fromUnicode("⬆️")).queue(success -> msg.addReaction(Emoji.fromUnicode("⬇️")).queue());
         });
-
+        
         return future;
     }
-
+    
     public static @Nullable TextChannel getSuggestionChannel(Guild guild) {
         final Bson serverConfigFilter = ServerConfigCommand.getFilter(guild);
         final GuildConfig config = ServerConfigCommand.get(serverConfigFilter, guild);
-        
+
         TextChannel channel = guild.getTextChannelById(config.getSuggestions());
         if (channel == null) {
-            channel = guild.getTextChannelsByName("suggestions", false).get(0);
-            if (channel == null)
+            final List<TextChannel> possibleChannels = guild.getTextChannelsByName("suggestions", false);
+            if (possibleChannels.isEmpty())
                 return null;
-        }
 
+            channel = possibleChannels.get(0);
+        }
+        
         if (!channel.canTalk())
             return null;
-
+        
         return channel;
     }
-
+    
     public static @Nullable TextChannel getSuggestionChannel(MessageReceivedEvent event) {
         final TextChannel channel = getSuggestionChannel(event.getGuild());
         if (channel == null) {
@@ -111,10 +113,10 @@ public final class SuggestionManager extends ListenerAdapter {
                 .queue();
             return null;
         }
-
+        
         return channel;
     }
-
+    
     public static @Nullable TextChannel getSuggestionChannel(SlashCommandInteractionEvent event) {
         final TextChannel channel = getSuggestionChannel(event.getGuild());
         if (channel == null) {
@@ -122,29 +124,29 @@ public final class SuggestionManager extends ListenerAdapter {
                 .mentionRepliedUser(false).mentionRepliedUser(false).queue();
             return null;
         }
-
+        
         return channel;
     }
-
+    
     public static CompletableFuture<Suggestion> respondSuggestion(Guild guild, TextChannel suggestionsChannel,
         Member responder, int number, String response, SuggestionResponse.Type type) {
         if (number < 0)
             return null;
         List<Suggestion> suggestions = new ArrayList<>();
         Database.getDatabase().suggestions.find(Filters.eq("guild", guild.getIdLong())).forEach(suggestions::add);
-
+        
         if (number > suggestions.size())
             return null;
-
+        
         suggestions = suggestions.stream().sorted(Comparator.comparing(Suggestion::getCreatedAt))
             .collect(Collectors.toList());
-
+        
         final long time = System.currentTimeMillis();
         final Suggestion suggestion = suggestions.get(number);
-
+        
         final Bson filter = Filters.and(Filters.eq("guild", guild.getIdLong()),
             Filters.eq("message", suggestion.getMessage()));
-
+        
         final var future = new CompletableFuture<Suggestion>();
         suggestionsChannel.retrieveMessageById(suggestion.getMessage()).queue(message -> {
             message.editMessageEmbeds(new EmbedBuilder(message.getEmbeds().get(0)).addField(
@@ -154,7 +156,7 @@ public final class SuggestionManager extends ListenerAdapter {
             Database.getDatabase().suggestions.updateOne(filter, Updates.set("responses", suggestion.getResponses()));
             future.complete(suggestion);
         }, err -> Database.getDatabase().suggestions.deleteOne(filter));
-
+        
         return future;
     }
 }
