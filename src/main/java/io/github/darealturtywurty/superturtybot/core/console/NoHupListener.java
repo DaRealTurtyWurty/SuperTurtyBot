ed5104fb8a176cd905d79c310332a9b3e019cc92
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-09-19 19:53:49 +0100

    More debugging 💀

--- src/main/java/io/github/darealturtywurty/superturtybot/core/console/NoHupListener.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/core/console/NoHupListener.java
@@ -11,35 +11,36 @@ import net.dv8tion.jda.api.JDA;
 
 public class NoHupListener {
     public void start(JDA jda) throws URISyntaxException {
+        System.out.println(isDevelopmentEnvironment());
         if (!isDevelopmentEnvironment())
             return;
-        
+
         Constants.LOGGER.debug("Not Development");
-        
+
         final var runFolder = new File(TurtyBot.class.getProtectionDomain().getCodeSource().getLocation().toURI())
             .getParentFile();
         Constants.LOGGER.debug(runFolder.toString());
         if (!runFolder.exists())
             return;
-        
+
         Constants.LOGGER.debug("Found run folder");
-        
+
         final Path projectFolder = Path.of(runFolder.getAbsolutePath()).getParent().getParent();
         Constants.LOGGER.debug(projectFolder.toString());
         final Path nohupOut = Path.of(projectFolder.toString(), "nohup.out");
         Constants.LOGGER.debug(nohupOut.toString());
         if (!Files.exists(nohupOut))
             return;
-        
-        Constants.LOGGER.debug("nohup.out exists");
 
+        Constants.LOGGER.debug("nohup.out exists");
+        
         final var monitor = new FileMonitor(1000);
         monitor.monitor(projectFolder.toString(), "nohup.out", new FileChangedAdapter(jda));
         monitor.start();
-        
+
         Constants.LOGGER.debug("File Monitor started");
     }
-    
+
     private static boolean isDevelopmentEnvironment() {
         return System.getenv("development") != null;
     }
