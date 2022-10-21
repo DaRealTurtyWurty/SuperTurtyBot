c74c4cf7bde83dd0704663060791cc7b39a04e06
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-10-21 22:57:46 +0100

    Role Selection (WIP)

--- src/main/java/io/github/darealturtywurty/superturtybot/commands/core/config/Validators.java
+++ src/main/java/io/github/darealturtywurty/superturtybot/commands/core/config/Validators.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.function.BiPredicate;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Message.MentionType;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -14,20 +15,23 @@ public final class Validators {
         TextChannel channel = guild.getTextChannelById(str);
         if (channel != null)
             return true;
-        
+
         if (MentionType.CHANNEL.getPattern().matcher(str).matches()) {
             final String id = str.replace("<#", "").replace(">", "");
             channel = guild.getTextChannelById(id);
             return channel != null;
         }
-        
+
         List<TextChannel> possibleMatches = guild.getTextChannelsByName(str, false);
         if (possibleMatches.isEmpty()) {
             possibleMatches = guild.getTextChannelsByName(str, true);
             if (possibleMatches.isEmpty())
                 return false;
         }
-        
+
         return true;
     };
+
+    public static final BiPredicate<SlashCommandInteractionEvent, String> EMOJI_VALIDATOR = (event,
+        str) -> Message.MentionType.EMOJI.getPattern().matcher(str).matches();
 }
