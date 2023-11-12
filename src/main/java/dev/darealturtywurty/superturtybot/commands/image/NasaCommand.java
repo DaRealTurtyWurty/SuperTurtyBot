d961672d1d3608a224ca3828da923e8cd8dc91f8
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2023-11-12 17:56:30 +0000

    /image type:nasa cleanup

--- src/main/java/dev/darealturtywurty/superturtybot/commands/image/NasaCommand.java
+++ src/main/java/dev/darealturtywurty/superturtybot/commands/image/NasaCommand.java
@@ -3,6 +3,7 @@ package dev.darealturtywurty.superturtybot.commands.image;
 import com.google.gson.JsonObject;
 import dev.darealturtywurty.superturtybot.Environment;
 import dev.darealturtywurty.superturtybot.core.util.Constants;
+import lombok.Data;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import okhttp3.OkHttpClient;
@@ -18,23 +19,19 @@ public class NasaCommand implements BiConsumer<SlashCommandInteractionEvent, Ima
     @Override
     public void accept(SlashCommandInteractionEvent event, ImageCommandType imageCommandType) {
         try {
-
-            if(Environment.INSTANCE.nasaApiKey().isEmpty()) {
+            if (Environment.INSTANCE.nasaApiKey().isEmpty()) {
                 Constants.LOGGER.warn("Nasa API key is not set!");
                 event.reply("❌ This command has been disabled by the bot owner!").mentionRepliedUser(false).queue();
                 return;
             }
 
-            final String NasaUrl = "https://api.nasa.gov/planetary/apod?api_key=%s"
-                    .formatted(Environment.INSTANCE.nasaApiKey().get());
-
-
-
             event.deferReply().queue();
-            final Request request = new Request.Builder().url(NasaUrl).get().build();
 
-            try(Response response = new OkHttpClient().newCall(request).execute()){
-                if(!response.isSuccessful()) {
+            final String nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=%s"
+                    .formatted(Environment.INSTANCE.nasaApiKey().get());
+            final Request request = new Request.Builder().url(nasaUrl).get().build();
+            try (Response response = new OkHttpClient().newCall(request).execute()) {
+                if (!response.isSuccessful()) {
                     event.getHook()
                             .sendMessage("❌ Failed to get response!")
                             .mentionRepliedUser(false)
@@ -60,13 +57,10 @@ public class NasaCommand implements BiConsumer<SlashCommandInteractionEvent, Ima
                     return;
                 }
 
-
-
-                Nasa nasaResponse = Nasa.fromJsonString(bodyString);
-
+                NasaResponseData nasaResponseDataResponse = NasaResponseData.fromJsonString(bodyString);
                 var embed = new EmbedBuilder()
-                        .setTitle(nasaResponse.title)
-                        .setImage(nasaResponse.hdurl)
+                        .setTitle(nasaResponseDataResponse.getTitle())
+                        .setImage(nasaResponseDataResponse.getHdurl())
                         .setFooter("Requested by " + event.getUser().getEffectiveName(), event.getUser().getEffectiveAvatarUrl())
                         .setTimestamp(Instant.now())
                         .setColor(0x2278c2)
@@ -77,13 +71,17 @@ public class NasaCommand implements BiConsumer<SlashCommandInteractionEvent, Ima
                         .mentionRepliedUser(false)
                         .queue();
             }
-        }catch(final IOException exception){
+        } catch (final IOException exception) {
             Constants.LOGGER.error("❌ Something went wrong with the NasaCommand!", exception);
-            event.reply("❌ There has been an issue gathering this astronomy image.")
-                    .mentionRepliedUser(false).queue();
+            event.getHook()
+                    .sendMessage("❌ Something went wrong with the NasaCommand!")
+                    .mentionRepliedUser(false)
+                    .queue();
         }
     }
-    public static class Nasa{
+
+    @Data
+    public static class NasaResponseData {
         private String copyright;
         private String date;
         private String explanation;
@@ -93,10 +91,10 @@ public class NasaCommand implements BiConsumer<SlashCommandInteractionEvent, Ima
         private String service_version;
         private String title;
         private String url;
-        private static Nasa fromJsonString(String json) {
-            JsonObject object = Constants.GSON.fromJson(json, JsonObject.class);
-            return Constants.GSON.fromJson(object, Nasa.class);
-        }
 
+        private static NasaResponseData fromJsonString(String json) {
+            JsonObject object = Constants.GSON.fromJson(json, JsonObject.class);
+            return Constants.GSON.fromJson(object, NasaResponseData.class);
+        }
     }
 }
