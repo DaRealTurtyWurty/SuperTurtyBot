58997ddea9bcc832cba3bf851933e117775a170b
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2024-03-25 21:58:01 +0000

    Update /latest parchment to be versioned

--- src/main/java/dev/darealturtywurty/superturtybot/commands/util/LatestCommand.java
+++ src/main/java/dev/darealturtywurty/superturtybot/commands/util/LatestCommand.java
@@ -8,7 +8,11 @@ import dev.darealturtywurty.superturtybot.core.util.function.Either;
 import dev.darealturtywurty.superturtybot.core.util.object.CoupledPair;
 import io.javalin.http.HttpStatus;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.AutoCompleteQuery;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
@@ -21,13 +25,68 @@ public class LatestCommand extends CoreCommand {
         super(new Types(true, false, false, false));
     }
 
+    @NotNull
+    private static EmbedBuilder createAllEmbed() {
+        Either<CoupledPair<MinecraftVersion>, HttpStatus> minecraftResponse = ApiHandler.getLatestMinecraft();
+        Either<CoupledPair<ForgeVersion>, HttpStatus> forgeResponse = ApiHandler.getLatestForge();
+        Either<CoupledPair<FabricVersion>, HttpStatus> fabricResponse = ApiHandler.getLatestFabric();
+        Either<CoupledPair<QuiltVersion>, HttpStatus> quiltResponse = ApiHandler.getLatestQuilt();
+        Either<CoupledPair<NeoforgeVersion>, HttpStatus> neoforgeResponse = ApiHandler.getLatestNeoforge();
+        Either<ParchmentVersion, HttpStatus> parchmentResponse = ApiHandler.getLatestParchment();
+
+        var embed = new EmbedBuilder();
+        if (minecraftResponse.isLeft()) {
+            CoupledPair<MinecraftVersion> versions = minecraftResponse.getLeft();
+            embed.addField("Minecraft", "Release: " + versions.getLeft().version() + "\nSnapshot: " + versions.getRight().version(), false);
+        } else {
+            embed.addField("Minecraft", "Failed to get latest Minecraft versions!", false);
+        }
+
+        if (forgeResponse.isLeft()) {
+            CoupledPair<ForgeVersion> versions = forgeResponse.getLeft();
+            embed.addField("Forge", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
+        } else {
+            embed.addField("Forge", "Failed to get latest Forge versions!", false);
+        }
+
+        if (fabricResponse.isLeft()) {
+            CoupledPair<FabricVersion> versions = fabricResponse.getLeft();
+            embed.addField("Fabric", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
+        } else {
+            embed.addField("Fabric", "Failed to get latest Fabric versions!", false);
+        }
+
+        if (quiltResponse.isLeft()) {
+            CoupledPair<QuiltVersion> versions = quiltResponse.getLeft();
+            embed.addField("Quilt", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
+        } else {
+            embed.addField("Quilt", "Failed to get latest Quilt versions!", false);
+        }
+
+        if (neoforgeResponse.isLeft()) {
+            CoupledPair<NeoforgeVersion> versions = neoforgeResponse.getLeft();
+            embed.addField("NeoForge", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
+        } else {
+            embed.addField("NeoForge", "Failed to get latest NeoForge versions!", false);
+        }
+
+        if (parchmentResponse.isLeft()) {
+            ParchmentVersion version = parchmentResponse.getLeft();
+            embed.addField("Parchment", "Version: " + version.version() + "\nMinecraft Version: " + version.version().split("-")[1], false);
+        } else {
+            embed.addField("Parchment", "Failed to get latest Parchment version!", false);
+        }
+        return embed;
+    }
+
     @Override
     public List<SubcommandData> createSubcommands() {
         return List.of(new SubcommandData("minecraft", "Get the latest Minecraft version"),
                 new SubcommandData("forge", "Get the latest Forge version"),
                 new SubcommandData("fabric", "Get the latest Fabric version"),
                 new SubcommandData("quilt", "Get the latest Quilt version"),
-                new SubcommandData("parchment", "Get the latest Parchment version"),
+                new SubcommandData("parchment", "Get the latest Parchment version")
+                        .addOption(OptionType.STRING, "version", "The version of Minecraft to get the latest Parchment version for", false, true),
                 new SubcommandData("neoforge", "Get the latest NeoForge version"),
                 new SubcommandData("all", "Get the latest versions of all"));
     }
@@ -59,7 +118,7 @@ public class LatestCommand extends CoreCommand {
 
     @Override
     protected void runSlash(SlashCommandInteractionEvent event) {
-        if(event.getSubcommandName() == null) {
+        if (event.getSubcommandName() == null) {
             reply(event, "❌ You must specify a subcommand!", false, true);
             return;
         }
@@ -144,75 +203,70 @@ public class LatestCommand extends CoreCommand {
                 event.getHook().editOriginalEmbeds(embed.build()).queue();
             }
             case "parchment" -> {
-                Either<ParchmentVersion, HttpStatus> response = ApiHandler.getLatestParchment();
+                OptionMapping versionOption = event.getOption("version");
+                if (versionOption == null) {
+                    Either<ParchmentVersion, HttpStatus> response = ApiHandler.getLatestParchment();
+                    if (response.isRight()) {
+                        event.getHook().sendMessage("❌ Failed to get latest Parchment version!").queue();
+                        return;
+                    }
+
+                    ParchmentVersion version = response.getLeft();
+                    var embed = new EmbedBuilder()
+                            .setTitle("Latest Parchment Version")
+                            .addField("Version", version.version(), true)
+                            .addField("Minecraft Version", version.version().split("-")[1], true);
+
+                    event.getHook().editOriginalEmbeds(embed.build()).queue();
+                    return;
+                }
+
+                String suppliedVersion = versionOption.getAsString().trim().replaceAll("[^0-9.]", "");
+                Either<ParchmentVersion, HttpStatus> response = ApiHandler.getParchment(suppliedVersion);
                 if (response.isRight()) {
+                    if (response.getRight() == HttpStatus.NOT_FOUND) {
+                        event.getHook().sendMessage("❌ There is no Parchment version for that Minecraft version!").queue();
+                        return;
+                    }
+
                     event.getHook().sendMessage("❌ Failed to get latest Parchment version!").queue();
                     return;
                 }
 
                 ParchmentVersion version = response.getLeft();
-                var embed = new EmbedBuilder()
-                        .setTitle("Latest Parchment Version")
-                        .addField("Version", version.version(), true)
-                        .addField("Minecraft Version", version.version().split("-")[1], true);
-
-                event.getHook().editOriginalEmbeds(embed.build()).queue();
+                event.getHook().editOriginal("`" + version.version() + "-" + suppliedVersion + "`").queue();
             }
             case "all" -> event.getHook().editOriginalEmbeds(createAllEmbed().build()).queue();
         }
     }
 
-    @NotNull
-    private static EmbedBuilder createAllEmbed() {
-        Either<CoupledPair<MinecraftVersion>, HttpStatus> minecraftResponse = ApiHandler.getLatestMinecraft();
-        Either<CoupledPair<ForgeVersion>, HttpStatus> forgeResponse = ApiHandler.getLatestForge();
-        Either<CoupledPair<FabricVersion>, HttpStatus> fabricResponse = ApiHandler.getLatestFabric();
-        Either<CoupledPair<QuiltVersion>, HttpStatus> quiltResponse = ApiHandler.getLatestQuilt();
-        Either<CoupledPair<NeoforgeVersion>, HttpStatus> neoforgeResponse = ApiHandler.getLatestNeoforge();
-        Either<ParchmentVersion, HttpStatus> parchmentResponse = ApiHandler.getLatestParchment();
+    @Override
+    public void onCommandAutoCompleteInteraction(@NotNull CommandAutoCompleteInteractionEvent event) {
+        if (!event.getName().equals(getName()) || event.getSubcommandName() == null || !event.getSubcommandName().equalsIgnoreCase("parchment"))
+            return;
 
-        var embed = new EmbedBuilder();
-        if (minecraftResponse.isLeft()) {
-            CoupledPair<MinecraftVersion> versions = minecraftResponse.getLeft();
-            embed.addField("Minecraft", "Release: " + versions.getLeft().version() + "\nSnapshot: " + versions.getRight().version(), false);
-        } else {
-            embed.addField("Minecraft", "Failed to get latest Minecraft versions!", false);
+        if(RATELIMITS.containsKey(event.getUser().getIdLong()) && RATELIMITS.get(event.getUser().getIdLong()).getRight() > System.currentTimeMillis()){
+            event.replyChoices().queue();
+            return;
         }
 
-        if (forgeResponse.isLeft()) {
-            CoupledPair<ForgeVersion> versions = forgeResponse.getLeft();
-            embed.addField("Forge", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
-        } else {
-            embed.addField("Forge", "Failed to get latest Forge versions!", false);
-        }
+        AutoCompleteQuery query = event.getFocusedOption();
+        if (query.getName().equals("version")) {
+            Either<List<MinecraftVersion>, HttpStatus> response = ApiHandler.getAllMinecraft();
+            if (response.isRight()) {
+                event.replyChoices().queue();
+                return;
+            }
 
-        if (fabricResponse.isLeft()) {
-            CoupledPair<FabricVersion> versions = fabricResponse.getLeft();
-            embed.addField("Fabric", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
-        } else {
-            embed.addField("Fabric", "Failed to get latest Fabric versions!", false);
+            List<MinecraftVersion> versions = response.getLeft();
+            String value = query.getValue();
+            event.replyChoiceStrings(versions.stream()
+                            .filter(MinecraftVersion::isRelease)
+                            .map(MinecraftVersion::version)
+                            .filter(version -> version.contains(value))
+                            .limit(25)
+                            .toList())
+                    .queue();
         }
-
-        if (quiltResponse.isLeft()) {
-            CoupledPair<QuiltVersion> versions = quiltResponse.getLeft();
-            embed.addField("Quilt", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
-        } else {
-            embed.addField("Quilt", "Failed to get latest Quilt versions!", false);
-        }
-
-        if (neoforgeResponse.isLeft()) {
-            CoupledPair<NeoforgeVersion> versions = neoforgeResponse.getLeft();
-            embed.addField("NeoForge", "Stable: " + versions.getLeft().version() + "\nLatest: " + versions.getRight().version(), false);
-        } else {
-            embed.addField("NeoForge", "Failed to get latest NeoForge versions!", false);
-        }
-
-        if (parchmentResponse.isLeft()) {
-            ParchmentVersion version = parchmentResponse.getLeft();
-            embed.addField("Parchment", "Version: " + version.version() + "\nMinecraft Version: " + version.version().split("-")[1], false);
-        } else {
-            embed.addField("Parchment", "Failed to get latest Parchment version!", false);
-        }
-        return embed;
     }
 }
