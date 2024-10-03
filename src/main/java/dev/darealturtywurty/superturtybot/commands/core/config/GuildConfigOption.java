f0d8891970049d831d138af1b2246a808b60dbc2
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2024-10-03 12:35:55 +0100

    Guild config autocomplete (WIP)

--- src/main/java/dev/darealturtywurty/superturtybot/commands/core/config/GuildConfigOption.java
+++ src/main/java/dev/darealturtywurty/superturtybot/commands/core/config/GuildConfigOption.java
@@ -6,14 +6,19 @@ import com.google.common.primitives.Floats;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
 import dev.darealturtywurty.superturtybot.database.pojos.collections.GuildData;
+import dev.darealturtywurty.superturtybot.database.pojos.collections.UserConfig;
 import dev.darealturtywurty.superturtybot.registry.Registerable;
 import lombok.Getter;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.internal.utils.Checks;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.text.WordUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
@@ -26,12 +31,15 @@ public class GuildConfigOption implements Registerable {
     private final BiPredicate<SlashCommandInteractionEvent, String> validator;
     @Getter
     private final Function<GuildData, Object> valueFromConfig;
+    @Getter
+    private final Function<String, List<Pair<String, String>>> autoComplete;
     
     private GuildConfigOption(Builder builder) {
         this.dataType = builder.dataType;
         this.serializer = builder.serializer;
         this.validator = builder.validator;
         this.valueFromConfig = builder.valueFromConfig;
+        this.autoComplete = builder.autoComplete;
     }
 
     @Override
@@ -67,6 +75,7 @@ public class GuildConfigOption implements Registerable {
         };
         private BiPredicate<SlashCommandInteractionEvent, String> validator = (dataType, str) -> true;
         private Function<GuildData, Object> valueFromConfig = config -> null;
+        private Function<String, List<Pair<String, String>>> autoComplete = str -> new ArrayList<>();
 
         public GuildConfigOption build() {
             return new GuildConfigOption(this);
@@ -93,6 +102,41 @@ public class GuildConfigOption implements Registerable {
         public Builder valueFromConfig(@NotNull Function<GuildData, Object> valueFromConfig) {
             Checks.notNull(valueFromConfig, "valueFromConfig");
             this.valueFromConfig = valueFromConfig;
+            return this;
+        }
+
+        public GuildConfigOption.Builder autoComplete(@NotNull Function<String, List<Pair<String, String>>> autoComplete) {
+            Checks.notNull(autoComplete, "autoComplete");
+            this.autoComplete = autoComplete;
+            return this;
+        }
+
+        public GuildConfigOption.Builder enumAutoComplete(@NotNull Enum<?>[] values) {
+            Checks.notNull(values, "values");
+            this.autoComplete = str -> {
+                List<Pair<String, String>> options = new ArrayList<>();
+                for (Enum<?> value : values) {
+                    String name = value.name().toLowerCase();
+                    if (!name.startsWith(str.toLowerCase()))
+                        continue;
+
+                    options.add(Pair.of(name.substring(0, 1).toUpperCase() + name.substring(1), name.toUpperCase()));
+                }
+
+                return options;
+            };
+            return this;
+        }
+
+        public GuildConfigOption.Builder autoCompleteColors() {
+            this.autoComplete = str -> {
+                List<Pair<String, String>> completions = new ArrayList<>();
+                for (Map.Entry<String, String> color : UserConfig.COLOR_HEX_MAP.entrySet()) {
+                    completions.add(Pair.of(color.getKey(), color.getValue()));
+                }
+
+                return completions;
+            };
             return this;
         }
     }
