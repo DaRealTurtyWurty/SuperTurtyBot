05fcc068bf5e151b785f1bbb7737ca969a03208f
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2024-11-04 19:16:47 +0000

    Fix thread manager sometimes not working
    - Pretty sure it will work this time

--- src/main/java/dev/darealturtywurty/superturtybot/modules/ThreadManager.java
+++ src/main/java/dev/darealturtywurty/superturtybot/modules/ThreadManager.java
@@ -7,6 +7,7 @@ import dev.darealturtywurty.superturtybot.database.pojos.collections.GuildData;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageType;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
@@ -22,43 +23,6 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadManager extends ListenerAdapter {
     public static final ThreadManager INSTANCE = new ThreadManager();
-
-    @Override
-    public void onChannelCreate(ChannelCreateEvent event) {
-        if (event.getChannelType() != ChannelType.GUILD_PUBLIC_THREAD || !event.isFromGuild())
-            return;
-
-        final Guild guild = event.getGuild();
-        GuildData config = Database.getDatabase().guildData.find(Filters.eq("guild", guild.getIdLong())).first();
-        if (config == null) {
-            config = new GuildData(guild.getIdLong());
-            Database.getDatabase().guildData.insertOne(config);
-        }
-
-        final ThreadChannel thread = event.getChannel().asThreadChannel();
-        thread.addThreadMember(guild.getSelfMember()).queue(RestAction.getDefaultSuccess(),
-                throwable -> Constants.LOGGER.error("Unable to add myself to a thread!", throwable));
-
-        if (!config.isShouldModeratorsJoinThreads())
-            return;
-
-        long ownerId = guild.getOwnerIdLong();
-        thread.sendMessage("Beans").setAllowedMentions(List.of()).queue(message -> {
-            var mentions = new StringBuilder("<@" + ownerId + ">");
-            guild.getRoles().stream()
-                    .filter(role -> role.hasPermission(Permission.MANAGE_THREADS) || role.hasPermission(Permission.MESSAGE_MANAGE))
-                    .map(guild::findMembersWithRoles)
-                    .forEach(task -> task.onSuccess(members -> {
-                        members.stream().map(Member::getAsMention).forEach(mentions::append);
-
-                        message.editMessage(mentions).queueAfter(2, TimeUnit.SECONDS,
-                                msg -> msg.delete().queueAfter(2, TimeUnit.SECONDS,
-                                        RestAction.getDefaultSuccess(),
-                                        throwable -> Constants.LOGGER.error("Failed to delete message!", throwable)),
-                                throwable -> Constants.LOGGER.error("Failed to send message to thread!", throwable));
-                    }).onError(throwable -> Constants.LOGGER.error("Failed to get members with roles!", throwable)));
-        }, throwable -> Constants.LOGGER.error("Failed to send message to thread!", throwable));
-    }
 
     @Override
     public void onThreadRevealed(@NotNull ThreadRevealedEvent event) {
@@ -94,9 +58,47 @@ public class ThreadManager extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.isFromGuild() || event.isFromThread() || event.isWebhookMessage() || event.getAuthor()
+        if (!event.isFromGuild() || event.isWebhookMessage() || event.getAuthor()
                 .isBot() || event.getAuthor().isSystem() || event.getMessage().getContentRaw()
                 .isBlank() || event.getMessage().getType().isSystem()) return;
+
+        if(event.isFromThread() && event.getMessage().getIdLong() == event.getChannel().getIdLong()) {
+            final Guild guild = event.getGuild();
+            GuildData config = Database.getDatabase().guildData.find(Filters.eq("guild", guild.getIdLong())).first();
+            if (config == null) {
+                config = new GuildData(guild.getIdLong());
+                Database.getDatabase().guildData.insertOne(config);
+            }
+
+            final ThreadChannel thread = event.getChannel().asThreadChannel();
+            thread.addThreadMember(guild.getSelfMember()).queue(RestAction.getDefaultSuccess(),
+                    throwable -> Constants.LOGGER.error("Unable to add myself to a thread!", throwable));
+
+            if (!config.isShouldModeratorsJoinThreads())
+                return;
+
+            long ownerId = guild.getOwnerIdLong();
+            thread.sendMessage("Beans").setAllowedMentions(List.of()).queue(message -> {
+                var mentions = new StringBuilder("<@" + ownerId + ">");
+                guild.getRoles().stream()
+                        .filter(role -> role.hasPermission(Permission.MANAGE_THREADS) || role.hasPermission(Permission.MESSAGE_MANAGE))
+                        .map(guild::findMembersWithRoles)
+                        .forEach(task -> task.onSuccess(members -> {
+                            members.stream().map(Member::getAsMention).forEach(mentions::append);
+
+                            message.editMessage(mentions).queueAfter(2, TimeUnit.SECONDS,
+                                    msg -> msg.delete().queueAfter(2, TimeUnit.SECONDS,
+                                            RestAction.getDefaultSuccess(),
+                                            throwable -> Constants.LOGGER.error("Failed to delete message!", throwable)),
+                                    throwable -> Constants.LOGGER.error("Failed to send message to thread!", throwable));
+                        }).onError(throwable -> Constants.LOGGER.error("Failed to get members with roles!", throwable)));
+            }, throwable -> Constants.LOGGER.error("Failed to send message to thread!", throwable));
+
+            return;
+        }
+
+        if(event.isFromThread())
+            return;
 
         final Guild guild = event.getGuild();
         GuildData config = Database.getDatabase().guildData.find(Filters.eq("guild", guild.getIdLong()))
