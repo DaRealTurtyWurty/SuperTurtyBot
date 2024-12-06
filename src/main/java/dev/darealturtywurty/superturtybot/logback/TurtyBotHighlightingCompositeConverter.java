9e263eb086a1bfbc5bbbe1c4c3b7358166356d5e
7410 <85879080+O7410@users.noreply.github.com>
2024-12-06 22:27:11 +0200

    Remove minecraft reference from log

--- src/main/java/dev/darealturtywurty/superturtybot/logback/TurtyBotHighlightingCompositeConverter.java
+++ src/main/java/dev/darealturtywurty/superturtybot/logback/TurtyBotHighlightingCompositeConverter.java
@@ -2,26 +2,25 @@ package dev.darealturtywurty.superturtybot.logback;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ANSIConstants;
 import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
 
-import static ch.qos.logback.core.pattern.color.ANSIConstants.*;
-
-public class MinecraftHighlightingCompositeConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+public class TurtyBotHighlightingCompositeConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
 
     @Override
     protected String getForegroundColorCode(ILoggingEvent event) {
         Level level = event.getLevel();
         if (getFirstOption().equals("thread")) {
             return switch (level.toInt()) {
-                case Level.ERROR_INT -> RED_FG;
-                case Level.WARN_INT -> YELLOW_FG;
-                case Level.INFO_INT -> GREEN_FG;
-                case Level.DEBUG_INT -> GREEN_FG;
-                case Level.TRACE_INT -> BLUE_FG;
+                case Level.ERROR_INT -> ANSIConstants.RED_FG;
+                case Level.WARN_INT -> ANSIConstants.YELLOW_FG;
+                case Level.INFO_INT -> ANSIConstants.GREEN_FG;
+                case Level.DEBUG_INT -> ANSIConstants.GREEN_FG;
+                case Level.TRACE_INT -> ANSIConstants.BLUE_FG;
                 default -> "";
             };
         } else if (getFirstOption().equals("msg")) {
-            return level.toInt() == Level.ERROR_INT ? RED_FG : "";
+            return level.toInt() == Level.ERROR_INT ? ANSIConstants.RED_FG : "";
         }
         return "";
     }
