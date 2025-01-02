415c076f7e2c5785902abefe38e21c0e5aa5b9df
7410 <85879080+O7410@users.noreply.github.com>
2025-01-02 12:47:24 +0200

    Loan changes

--- src/main/java/dev/darealturtywurty/superturtybot/modules/economy/EconomyManager.java
+++ src/main/java/dev/darealturtywurty/superturtybot/modules/economy/EconomyManager.java
@@ -330,12 +330,10 @@ public class EconomyManager {
         return loan;
     }
 
-    public static boolean payLoan(Economy account, Loan loan, long amount) {
+    public static void payLoan(Economy account, Loan loan, long amount) {
         long returned = loan.pay(amount);
         removeMoney(account, amount - returned, true);
         updateAccount(account);
-
-        return loan.isPaidOff();
     }
 
     public static long getTimeToPayOff(long amount) {
